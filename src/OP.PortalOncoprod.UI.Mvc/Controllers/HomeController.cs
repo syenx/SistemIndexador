539920c2423950e809116d04896d264d44d5a618
MARCELO FRANCO <marcelofranco.ti@gmail.com>
2022-03-15 09:11:50 -0300

    Lista de arquivos enviados com sucesso

--- src/OP.PortalOncoprod.UI.Mvc/Controllers/HomeController.cs
+++ src/OP.PortalOncoprod.UI.Mvc/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+using OP.PortalOncoprod.UI.Mvc.Models;
 
 namespace SistemaIndexador.UI.Mvc.Controllers
 {
@@ -27,7 +32,26 @@ namespace SistemaIndexador.UI.Mvc.Controllers
 
         public void upload(string data)
         {
-        
+
+            string directory = @"C:\Temp\UploadIndexador\old\";
+
+            for (int i = 0; i < Request.Files.Keys.Count; i++)
+            {
+
+                HttpPostedFileBase file = Request.Files[i];
+
+                if (file != null && file.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(file.FileName);
+
+                    file.SaveAs(Path.Combine(directory, fileName));
+                }
+            }
+
+
+
+
+
             ViewBag.Message = "Your contact page.";
         }
     }
---
+++ src/OP.PortalOncoprod.UI.Mvc/Models/ArquivosPDF.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OP.PortalOncoprod.UI.Mvc.Models
+{
+    public class ArquivosPDF
+    {
+        [JsonProperty(PropertyName = "id")]
+        public int id { get; set; }
+        [JsonProperty(PropertyName = "nome")]
+        public string nome { get; set; }
+    }
+}
