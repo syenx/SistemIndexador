0aa10167a448a4b650f25bc2b5b2b75f25834605
MARCELO FRANCO <marcelofranco.ti@gmail.com>
2022-03-25 13:46:59 -0300

    Ajustes das classes faltantes

---
+++ src/OP.PortalOncoprod.UI.Mvc/Controllers/VisualizadorController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SistemaIndexador.Application.ViewModels;
+
+namespace OP.PortalOncoprod.UI.Mvc.Controllers
+{
+    public class VisualizadorController : Controller
+    {
+        // GET: Visualizador
+        public ActionResult Index()
+        {
+            DadosIndexacaoViewModel model = new DadosIndexacaoViewModel();
+            model.ListaAquivos = ListarArquivosParaIndexar();
+
+            return View(model);
+        }
+
+        private List<string> ListarArquivosParaIndexar()
+        {
+
+            DirectoryInfo DirOld = new DirectoryInfo(@"C:\Temp\UploadIndexador\new\");
+            List<string> lista = new List<string>();
+
+
+            FileInfo[] Files = DirOld.GetFiles("*", SearchOption.AllDirectories);
+            foreach (FileInfo File in Files)
+            {
+                string FileName = @"C:\Temp\UploadIndexador\new\" + File.FullName.Replace(DirOld.FullName, "");
+                lista.Add(FileName);
+            }
+
+            return lista;
+
+        }
+    }
+}
